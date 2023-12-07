28a8d318929a89370f34f37c2d0d3560e4344595
a2823kevin <lireez404@gmail.com>
2023-12-07 19:20:10 +0800

    add .env

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 
+source .env
 source venv/bin/activate
-uvicorn --app-dir=src html2pdfserver:app --reload
+uvicorn --host 0.0.0.0 --port $SERVER_PORT --app-dir=src html2pdfserver:app --reload
